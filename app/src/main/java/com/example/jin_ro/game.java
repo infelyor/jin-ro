503ff239088821159dda33d553d1502ba64bcaf8
infelyor <takunyann0@gmail.com>
2020-11-24 12:05:43 +0900

    11/24

--- app/src/main/java/com/example/jin_ro/game.java
+++ app/src/main/java/com/example/jin_ro/game.java
@@ -3,6 +3,8 @@ package com.example.jin_ro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class game extends AppCompatActivity {
 
@@ -11,5 +13,20 @@ public class game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        CountDownTimer cdt = new CountDownTimer(10000.100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int time = (int)millisUntilFinished /1000;
+
+                TextView textView = (TextView) findViewById(R.id.tm);
+                textView.setText(time);
+            }
+
+            @Override
+            public void onFinish() {
+                finish();
+            }
+        }
     }
 }
